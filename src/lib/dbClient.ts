ab34bf91a79ef4fbe2156bf40cd9730fd94eec77
Saikat Sardar <saikatsardar5@gmail.com>
2025-11-25 17:57:41 +0530

    Next.js Starter Fullstack Node Init

---
+++ src/lib/dbClient.ts
@@ -0,0 +1,19 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../../generated/prisma/client";
+import { serverEnv } from "./env/serverEnv";
+
+const globalForPrisma = globalThis as unknown as {
+	prisma?: PrismaClient;
+};
+
+const adapter = new PrismaBetterSqlite3({
+	url: serverEnv.DATABASE_URL,
+});
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+	globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
